04e999d99385e4d8cd1a52870ea018f934e12afe
dsjr-13 <durksteed13@gmail.com>
2022-04-30 22:36:44 -0400

    fix home page spacing

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 const Index = () => {
   return (
     <div className="absolute left-0 top-0 flex flex-col items-center justify-center space-y-8 h-full min-h-screen w-full min-w-[100vw] bg-gradient-to-br from-[#00E5FF] to-[#D500F9]">
-      <h1 className="font-bold text-4xl text-white">Influx UI</h1>
-      <p className="sub-text text-white">
+      <h1 className="mx-6 font-bold text-4xl text-white text-center">Influx UI</h1>
+      <p className="mx-6 sub-text text-white text-center">
         Influx UI is a component library built on top of Tailwind CSS.
       </p>
 
-      <div className="remark-highlight">
+      <div className="mx-6 remark-highlight max-w-[calc(100vw-3rem)]">
         <pre className="language-jsx rounded">
           <code className="language-jsx">
             <span className="token tag">
